2d1acca0bf8178e4614f9b4f730a719ca0e9e572
q10242 <q10242@gmail.com>
2023-10-26 16:54:53 +0800

    add static files

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,4 @@ RUN pip install -U pip
 RUN pip install --no-cache-dir -r requirements.txt
 
 
-ENTRYPOINT ["./docker-entrypoint.sh"]
+# ENTRYPOINT ["./docker-entrypoint.sh"]
--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 import uvicorn
 from fastapi import FastAPI, Request
-
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
+
+@app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     """
     首頁
     """
-    return {"msg": "Hello ", "code": 0}
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+    })
 
 
 def main():
